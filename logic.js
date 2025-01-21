c33b8402112ccd5564e5259a92220c81357bc55f
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2025-01-21 22:31:40 +0100

    interface cards

--- logic.js
+++ logic.js
@@ -2,16 +2,11 @@ import {
   cardDwarf_0, cardDwarf_1, cardDwarf_2, cardDwarf_3, cardDwarf_4, cardDwarf_5, cardDwarf_6, cardDwarf_7, cardDwarf_8, cardDwarf_9
 } from "./data.js";
 
-console.log(cardDwarf_4);
-
 const handCardContainerHTML = document.querySelector(".hand-card-container");
+const cardEmpPlayerHTML = document.querySelectorAll(".emplacement-player");
 
 
-
-
-
-
-for (let i = 0; i < 7; i++) {
+for (let i = 1; i < 7; i++) {
   const randomN = Math.floor(Math.random() * 10);
   const handCardInjection = `
   <div
@@ -21,15 +16,45 @@ for (let i = 0; i < 7; i++) {
   handCardContainerHTML.innerHTML += handCardInjection;
 };
 
+function placeCardOnEmplacement() {
+}
 
+let tempArr = [];
+let cardChoiceText = "";
+let actualCardArr = []
 setTimeout(() => {
-  const handCardItem = document.querySelectorAll(".hand-card-img");
-
-  console.log(handCardItem);
-
-  handCardItem.forEach(item => (
+  const handCardItems = document.querySelectorAll(".hand-card-img");
+  handCardItems.forEach(card => {
+    actualCardArr.push(handCardItems)
+    console.log(actualCardArr)
+  })
+  handCardItems.forEach(item => (
     item.addEventListener("click", function () {
+      tempArr = [];
       console.log(item)
+      item.classList.toggle("chosen");
+      tempArr.push(item.outerHTML);
+      cardChoiceText = tempArr[0];
+      console.log(cardChoiceText.replace("chosen", ""))
+
+      cardEmpPlayerHTML.forEach(empl => (
+        empl.addEventListener("click", function () {
+          empl.innerHTML = cardChoiceText.replace("chosen", "");
+
+          if (empl.innerHTML == null) {
+          }
+        })
+      ))
     })
   ))
-}, 2000);
+
+
+}, 2000)
+
+
+
+
+
+
+
+// placeCardOnEmplacement();
