fdd7b6d57928aede0193ac006747db6fbac62d33
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2025-01-27 02:22:10 +0100

    interaction turn player

--- logic.js
+++ logic.js
@@ -6,18 +6,37 @@ const data = [cardDwarf_0, cardDwarf_1, cardDwarf_2, cardDwarf_3, cardDwarf_4, c
 ]
 const handCardContainerHTML = document.querySelector(".hand-card-container");
 const cardEmpPlayerHTML = document.querySelectorAll(".emplacement-player");
+
 const cardEmpBotHTML = document.querySelectorAll(".emplacement-bot");
 
-// function handCardBot() {
-//   for (let i = 1; i < 4; i++) {
-//     const handCardInjectionBot = `
-//   <div
-//   class="hand-card playedCard-bot"><img src="assets/cards/testCardBgBot.png" alt="81" class="hand-card-img">
-//   </div>
-//   `;
-//     cardEmpBotHTML.innerHTML += handCardInjectionBot;
-//   }
-// }
+
+
+function InitializeBotCard() {
+  for (let i = 1; i < 4; i++) {
+    const handCardInjectionBot = `
+    <div class="hand-card playedCard-bot">
+                  <img
+                    src="assets/cards/testCardBgBot.png"
+                    alt="81"
+                    class="hand-card-img"
+                  />
+                </div>
+    `;
+    document.querySelector(`.bot-emplacement-card-${i}`).innerHTML = handCardInjectionBot;
+  }
+}
+
+function DisplayCardBot() {
+  for (let i = 1; i < 4; i++) {
+    const randomN = Math.floor(Math.random() * 10);
+    const handCardInjectionBot = `
+    <div
+    class="hand-card"><img src="${eval(`cardDwarf_${randomN}`).img}" alt="81" class="hand-card-img ">
+    </div>
+    `;
+    document.querySelector(`.bot-emplacement-card-${i}`).innerHTML = handCardInjectionBot;
+  }
+}
 
 let playerDeck = 30;
 const deckCount = document.querySelector(".deck-count");
@@ -61,7 +80,7 @@ let cardChoiceText = "";
 let actualCardArr = []
 let cardChoice = false;
 let counterEmpl = 0
-let objCardArr = [];
+
 setTimeout(() => {
   const handCardItems = document.querySelectorAll(".hand-card-img");
   handCardItems.forEach(card => {
@@ -109,6 +128,7 @@ setTimeout(() => {
           displayDataCard(empl, empl.id)
         }
 
+        validateTurnPlayer();
       }
     })
   ))
@@ -116,9 +136,9 @@ setTimeout(() => {
 }, 10)
 
 
-function displayDataCard(empl,emplId) {
+function displayDataCard(empl, emplId) {
   const fullSrc = empl.querySelector("img").src;
-  const baseUrl = "http://127.0.0.1:5500/";
+  const baseUrl = window.location.origin + "/";
   const itemSrc = fullSrc.replace(baseUrl, "");
   const objCard = data.filter(card => card.img == itemSrc);
   const emplIdSuppressor = "empl-"
@@ -127,13 +147,13 @@ function displayDataCard(empl,emplId) {
   if (objCard.length > 0) {
     const resultHTML = document.querySelector(`.result-player-${emplIdRecover}`);
     console.log(resultHTML, emplIdRecover)
-      const red = objCard[0].red;
-      const blue = objCard[0].blue;
-      const yellow = objCard[0].yellow;
-      console.log(`red: ${red}`, `blue: ${blue}`, `yellow: ${yellow}`)
-      document.querySelector(`.r-red-${emplIdRecover}`).textContent = red;
-      document.querySelector(`.r-blue-${emplIdRecover}`).textContent = blue;
-      document.querySelector(`.r-yellow-${emplIdRecover}`).textContent = yellow;
+    const red = objCard[0].red;
+    const blue = objCard[0].blue;
+    const yellow = objCard[0].yellow;
+    console.log(`red: ${red}`, `blue: ${blue}`, `yellow: ${yellow}`)
+    document.querySelector(`.player-red-${emplIdRecover}`).textContent = red;
+    document.querySelector(`.player-blue-${emplIdRecover}`).textContent = blue;
+    document.querySelector(`.player-yellow-${emplIdRecover}`).textContent = yellow;
   }
 }
 
@@ -164,6 +184,35 @@ function verifyEmplFree(item) {
 }
 
 
+function validateTurnPlayer() {
+  const cardEmpl1 = document.getElementById("empl-1");
+  const cardEmpl2 = document.getElementById("empl-2");
+  const cardEmpl3 = document.getElementById("empl-3");
+  let cardEmpl1Valid = false;
+  let cardEmpl2Valid = false;
+  let cardEmpl3Valid = false;
+
+  if (cardEmpl1.firstChild && cardEmpl1.firstChild.matches(".hand-card")) {
+    cardEmpl1Valid = true
+  }
+  if (cardEmpl2.firstChild &&
+    cardEmpl2.firstChild.matches(".hand-card")) {
+    cardEmpl2Valid = true
+  }
+  if (cardEmpl3.firstChild &&
+    cardEmpl3.firstChild.matches(".hand-card")) {
+    cardEmpl3Valid = true
+  }
+
+
+  console.log("Player turn done.")
+  DisplayCardBot();
+  // } else {
+  //   console.log("Player turn not done yet.")
+  // }
+}
+
 
 // placeCardOnEmplacement();
 initializeGame();
+InitializeBotCard();
